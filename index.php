3d17b96cbb4388394a0e9559b8b23fff7ee83dab
AFulvio1 <73074158+AFulvio1@users.noreply.github.com>
2022-04-28 22:49:18 +0200

    small fix

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 <html lang="en">
 
   <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-      <title>Lido</title>
+    <title>Lido</title>
 
-      <!-- Favicon -->
-      <link rel="shortcut icon" href="./static/img//favicon.ico" type="image/x-icon">
-      <link rel="icon" href="./static/img//favicon.ico" type="image/x-icon">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/static/img//favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./static/img//favicon.ico" type="image/x-icon">
 
-      <!-- Stylesheets -->
-      <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre Baskerville:wght@300;400;700;900&display=swap">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Voltaire:wght@300;400;700;900&display=swap">
-      <link rel="stylesheet" href="/static/css/style.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-      <link rel="stylesheet" href="/static/css/coockie.css" async>
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre Baskerville:wght@300;400;700;900&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Voltaire:wght@300;400;700;900&display=swap">
+    <link rel="stylesheet" href="/static/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="/static/css/coockie.css" async>
   </head>
 
   <body>
@@ -26,34 +26,34 @@
     <div data-bs-spy="scroll" data-bs-target="#navbar-container" data-bs-offset="0" class="scrollspy-example" tabindex="0">
     
     <!-- NAVBAR -->
-    <div id="navbar-container"></div>
-    <div class="load-html" data-container="navbar-container" data-source="/partial/navbar.html"></div>
+    <span id="navbar-container"></span>
+    <span class="load-html" data-container="navbar-container" data-source="/partial/navbar.html"></span>
         
     <!-- HERO -->
-    <div id="hero-container"></div>
-    <div class="load-html" data-container="hero-container" data-source="/partial/hero.html"></div>
+    <span id="hero-container"></span>
+    <span class="load-html" data-container="hero-container" data-source="/partial/hero.html"></span>
 
     <!-- POSTER -->
-    <div id="poster-container"></div>
-    <div class="load-html" data-container="poster-container" data-source="/partial/poster.html"></div>
+    <span id="poster-container"></span>
+    <span class="load-html" data-container="poster-container" data-source="/partial/poster.html"></span>
 
     <!-- SERVIZI -->
-    <div id="services-container"></div>
-    <div class="load-html" data-container="services-container" data-source="/partial/services.html"></div>
+    <span id="services-container"></span>
+    <span class="load-html" data-container="services-container" data-source="/partial/services.html"></span>
 
 
     <!-- GALLERIA-->
-    <div id="galleria-container"></div>
-    <div class="load-html" data-container="galleria-container" data-source="/partial/galleria.html"></div>
+    <span id="galleria-container"></span>
+    <span class="load-html" data-container="galleria-container" data-source="/partial/galleria.html"></span>
 
 
     <!-- CONTATTI DA FARE -->
-    <div id="contact-container"></div>
-    <div class="load-html" data-container="contact-container" data-source="/partial/contact.html"></div>
+    <span id="contact-container"></span>
+    <span class="load-html" data-container="contact-container" data-source="/partial/contact.html"></span>
 
     <!-- FOOTER DA FARE -->
-    <div id="footer-container"></div>
-    <div class="load-html" data-container="footer-container" data-source="/partial/footer.html"></div>
+    <span id="footer-container"></span>
+    <span class="load-html" data-container="footer-container" data-source="/partial/footer.html"></span>
       
       
     
--- pages/umbrellas.php
+++ pages/umbrellas.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-      <title>Prenota il tuo ombrellone!</title>
+    <title>Lido</title>
 
-      <!-- Favicon -->
-      <link rel="shortcut icon" href="./static/img//favicon.ico" type="image/x-icon">
-      <link rel="icon" href="./static/img//favicon.ico" type="image/x-icon">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/static/img//favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./static/img//favicon.ico" type="image/x-icon">
 
-      <!-- Stylesheets -->
-      <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&display=swap">
-      <link rel="stylesheet" href="/static/css/style.css">
-      <link rel="stylesheet" href="/static/css/coockie.css" async>
-  </head>
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre Baskerville:wght@300;400;700;900&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Voltaire:wght@300;400;700;900&display=swap">
+    <link rel="stylesheet" href="/static/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="/static/css/coockie.css" async>
+</head>
 <body>
 
     <!-- NAVBAR -->
-    <div id="navbar-container"></div>
-    <div class="load-html" data-container="navbar-container" data-source="/partial/navbar.html"></div>
+    <span id="navbar-container"></span>
+    <span class="load-html" data-container="navbar-container" data-source="/partial/navbar.html"></span>
 
     <h2>PRENOTA IL TUO OMBRELLONE</h2>
 
@@ -32,38 +34,45 @@
     ?>
    
     <!-- Inserire commenti -->
-    <?php
-        $n_umbrella = 400;
-        foreach($table as $i => $row) {
-            echo "<div class=\"flexbox\">";
-            foreach($row as $j => $column) {
-                echo "<div class=\"item\">";
-                if ($j == "ID") {
-                    continue;
-                }
-                else {
-                    if ($row[$j] == "t") {
-                        echo "<button type=\"button\" class=\"btn btn-success btn-umbrella\" onclick=\"buttonChoise()\">" . $n_umbrella . "</button>";
+    <div class="flexbox-container">
+        <?php
+            $n_umbrella = 400;
+            foreach($table as $i => $row) {
+                //echo "<div class=\"grid-container\">";
+                foreach($row as $j => $column) {
+                    if ($j == "ID") {
+                        continue;
                     }
                     else {
-                        echo "<button type=\"button\" class=\"btn btn-danger btn-umbrella\" onclick=\"buttonAlert()\">" . $n_umbrella . "</button>";
+                        echo "<div class=\"item\">";
+                        if ($row[$j] == "t") {
+                            echo "<button type=\"button\" class=\"btn btn-success btn-umbrella\" onclick=\"buttonChoise()\">" . $n_umbrella . "</button>";
+                        }
+                        else {
+                            echo "<button type=\"button\" class=\"btn btn-danger btn-umbrella\" onclick=\"buttonAlert()\">" . $n_umbrella . "</button>";
+                        }
+                        $n_umbrella -= 1;
+                        echo "</div>";
                     }
-                    $n_umbrella -= 1;
                 }
-                echo "</div>";
-            }
-            echo "</div>";
-        }      
-    ?>
+                //echo "</div>";
+            }      
+        ?>
+    </div>
+    
+    
 
 
 
     <!-- FOOTER DA FARE -->
-    <div id="footer-container"></div>
-    <div class="load-html" data-container="footer-container" data-source="/partial/footer.html"></div>
-    
-    
+    <span id="footer-container"></span>
+    <span class="load-html" data-container="footer-container" data-source="/partial/footer.html"></span>
 
+
+    
+    <!-- SCRIPTS
+    <span id="scripts-container"></span>
+    <span class="load-html" data-container="scripts-container" data-source="/partial/scripts.html"></span> -->
 
     <!-- Bootstrap -->
     <script src="/bootstrap/js/bootstrap.min.js"></script>
@@ -83,6 +92,33 @@
         });
     </script>
 
+    <!-- Buttons -->
+    <script>
+        function buttonChoise() {
+            /** choise: true if ok, false else */
+            var choise = confirm("Vuoi confermare la scelta?");
+        }
+        function buttonAlert() {
+            alert("Purtroppo l'ombrellone selezionato è stato già prenotato!");
+        }
+    </script>
+
+    <!-- Navbar -->
+    <script>
+        var nav = document.querySelector('nav');
+
+        window.addEventListener('scroll',function(){
+            if (window.pageYOffset > 100){
+                nav.classList.add('bg-dark','shadow');
+            }
+            else{
+                nav.classList.remove('bg-dark','shadow');
+            }
+        })
+    </script>
+      
+ 
+
     <!-- Load HTML -->
     <script>
         $(function () {
@@ -96,15 +132,6 @@
         });
     </script>
 
-    <!-- Buttons -->
-    <script>
-        function buttonChoise() {
-            /** choise: true if ok, false else */
-            var choise = confirm("Vuoi confermare la scelta?");
-        }
-        function buttonAlert() {
-            alert("Purtroppo l'ombrellone selezionato è stato già prenotato!");
-        }
-    </script>
+    
 </body>
 </html>
